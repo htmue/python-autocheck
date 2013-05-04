0ce9aeed7241810f8f602f925bc15b6149c4ed2d
Hans-Thomas Mueller <htmue@mac.com>
2013-05-05 00:40:59 +0200

    Added Django autocheck command.

---
+++ autocheck/management/commands/autocheck.py
@@ -0,0 +1,83 @@
+# -*- coding:utf-8 -*-
+# Created by Hans-Thomas on 2012-09-02.
+#=============================================================================
+#   autocheck.py --- Autocheck command
+#=============================================================================
+import os, sys
+from optparse import make_option
+
+from django.core.management import BaseCommand, call_command, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Run tests automatically'
+    option_list = (
+        make_option('--once', action='store_true', default=False,
+            help='Single test run.'
+        ),
+        make_option('--no-verbose', action='store_true', default=False,
+            help='Do not run unittests verbose.'
+        ),
+        make_option('--noinput', action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind.'
+        ),
+        make_option('-f', '--failfast', action='store_true', default=False,
+            help='Stop on first failure.'
+        ),
+        make_option('-c', '--catch', action='store_true', default=False,
+            help='Catch control-C and display results.'
+        ),
+        make_option('--no-buffer', action='store_true', default=False,
+            help='Do not buffer stdout and stderr during test runs.'
+        ),
+        make_option('-s', metavar='DIRECTORY', dest='start', default='.',
+            help='Directory to start discovery (default: ".").'
+        ),
+        make_option('-p', metavar='PATTERN', dest='pattern',
+            help='Pattern to match test files ("test*.py" default).'
+        ),
+        make_option('-t', metavar='DIRECTORY', dest='top', default='.',
+            help='Top level directory of project (default: ".").'
+        ),
+    ) + BaseCommand.option_list
+    requires_model_validation = False
+    can_import_settings = False
+    
+    def handle(self, *args, **options):
+        if options.pop('once'):
+            self.once(*args, **options)
+        else:
+            self.autotest(*args, **options)
+    
+    def once(self, *args, **options):
+        try:
+            call_command('clean_pyc')
+        except CommandError:
+            pass
+        from ...main import Unbuffered
+        sys.stdout = Unbuffered(sys.stdout)
+        from django.conf import settings
+        from django.test.utils import get_runner
+        verbosity = int(options.pop('verbosity', 1))
+        if not options.pop('no_verbose'):
+            options['verbose'] = True
+        if not options.pop('no_buffer'):
+            options['buffer'] = True
+        interactive = options.pop('interactive', True)
+        failfast = options.get('failfast', False)
+        TestRunner = get_runner(settings)
+        test_runner = TestRunner(verbosity=verbosity, interactive=interactive, failfast=failfast)
+        failures = test_runner.run_tests(args, **options)
+        if failures:
+            sys.exit(bool(failures))
+    
+    def autotest(self, *args, **options):
+        import resource
+        resource.setrlimit(resource.RLIMIT_NOFILE, (4096, -1))
+        os.environ['DJANGO_SETTINGS_MODULE'] = options['settings'] or 'test_settings'
+        from ...main import autocheck
+        autocheck(sys.argv)
+
+
+#.............................................................................
+#   autocheck.py
---
+++ autocheck/testsuiterunner.py
@@ -0,0 +1,60 @@
+# -*- coding:utf-8 -*-
+# Created by Hans-Thomas on 2011-05-20.
+#=============================================================================
+#   testsuiterunner.py --- Django test suite runner
+#=============================================================================
+from django.test.simple import DjangoTestSuiteRunner
+
+from autocheck.db import Database
+from autocheck.testrunner import TestRunner, TestProgram
+
+
+try:
+    from south.management.commands import patch_for_test_db_setup
+except ImportError:
+    pass
+else:
+    patch_for_test_db_setup()
+
+class TestSuiteRunner(DjangoTestSuiteRunner):
+    opts = dict(
+        failfast = '--failfast',
+        catch = '--catch',
+        buffer = '--buffer',
+        pattern = '-p',
+        verbose = '-v',
+    )
+    discover_opts = dict(
+        start = '-s',
+        top = '-t',
+    )
+    discover_opts.update(opts)
+    
+    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+        argv = ['./manage.py unittest']
+        discover = len(test_labels) == 0 or len(test_labels) == 1 and test_labels[0] == 'discover'
+        if discover:
+            argv.append('discover')
+            opts = self.discover_opts
+        else:
+            opts = self.opts
+        for key, value in kwargs.items():
+            if key in opts:
+                option = opts[key]
+                if value is True:
+                    argv.append(option)
+                elif value is not False and value is not None:
+                    argv.append(option)
+                    argv.append(value)
+        if not discover:
+            argv.extend(test_labels)
+        print ' '.join(argv)
+        self.setup_test_environment()
+        old_config = self.setup_databases()
+        result = TestProgram(module=None, argv=argv, testRunner=TestRunner, database=Database())
+        self.teardown_databases(old_config)
+        self.teardown_test_environment()
+        return self.suite_result(suite, result)
+
+#.............................................................................
+#   testsuiterunner.py
