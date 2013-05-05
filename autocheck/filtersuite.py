f691d9c1041ca5e0449ed47ac32175e9f96ffd55
Hans-Thomas Mueller <htmue@mac.com>
2013-05-05 17:40:48 +0200

    Sort test suite.

--- autocheck/filtersuite.py
+++ autocheck/filtersuite.py
@@ -3,6 +3,8 @@
 #=============================================================================
 #   filtersuite.py --- Filter test suites
 #=============================================================================
+from operator import methodcaller
+
 from compat import unittest
 
 
@@ -14,16 +16,16 @@ def flatten_suite(suite):
                     yield t
             else:
                 yield test
-    return unittest.TestSuite(flatten(suite))
+    return unittest.TestSuite(sorted(flatten(suite), key=methodcaller('id')))
 
 
 def filter_suite(suite, database):
     full_suite = True
+    suite = flatten_suite(suite)
     if database is not None:
-        flattened = flatten_suite(suite)
-        candidates = database.candidates(flattened)
+        candidates = database.candidates(suite)
         if candidates:
-            tests_to_run = [test for test in flattened if str(test) in candidates]
+            tests_to_run = [test for test in suite if str(test) in candidates]
             if tests_to_run:
                 suite = unittest.TestSuite(tests_to_run)
                 full_suite = False
