3d7fb94edfb873bebcd3d682485036f28000577f
Hans-Thomas Mueller <htmue@mac.com>
2015-04-19 22:42:21 +0200

    Added missing growler images to setup.py.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info[:2] == (2, 6):
 
 setup(
     name='autocheck',
-    version='0.2.2',
+    version='0.2.3',
     description='Improved unittest test runner',
     author='Hans-Thomas Mueller',
     author_email='htmue@mac.com',
@@ -26,7 +26,7 @@ setup(
         'autocheck.contrib.django.management',
         'autocheck.contrib.django.management.commands',
     ],
-    package_data=dict(autocheck=['*.yaml', 'colourschemes/*.yaml']),
+    package_data=dict(autocheck=['*.yaml', 'colourschemes/*.yaml', 'images/*.png']),
     scripts=['bin/autocheck'],
     install_requires=install_requires,
 )
